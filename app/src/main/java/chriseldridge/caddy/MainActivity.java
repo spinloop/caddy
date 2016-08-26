f24f22ded0b26d6f8aefedea64175592a12f6a76
Chris Eldridge <cre@mojotech.com>
2016-08-26 07:43:21 -0400

    setup qr code scanner

--- app/src/main/java/chriseldridge/caddy/MainActivity.java
+++ app/src/main/java/chriseldridge/caddy/MainActivity.java
@@ -1,18 +1,38 @@
 package chriseldridge.caddy;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView scannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        scannerView = new ZXingScannerView(this);
+        scannerView.setResultHandler(this);
     }
 
     public void QRScanner(View view) {
+        setContentView(scannerView);
+
+        scannerView.startCamera();
+    }
+    
+    @Override
+    public void handleResult(Result rawResult) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
     }
 }
